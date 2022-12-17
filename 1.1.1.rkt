6502b45dce6cd6b30f6150799d47075032d43339
iamb33k <99271420+iamb33k@users.noreply.github.com>
2022-12-17 18:37:31 -0500

    Add files via upload

---
+++ 1.1.1.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+;Section 1.1.1 -- Expressions
+
+;Expressions representing numbers combined with expressions represeneting primitives
+(+ 137 349)
+
+(- 1000 334)
+
+(* 5 99)
+
+(/ 10 5)
+
+(+ 2.7 10)
